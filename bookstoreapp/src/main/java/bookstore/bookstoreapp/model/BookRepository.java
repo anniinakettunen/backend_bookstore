edc3de5a0b6a4d4bb6405b93d88d25db3759b4d8
Anniina Kettunen <anniina.kettunen@gmail.com>
2025-09-04 20:04:15 +0300

    Harjoitus osa 1 valmis

--- bookstoreapp/src/main/java/bookstore/bookstoreapp/model/BookRepository.java
+++ bookstoreapp/src/main/java/bookstore/bookstoreapp/model/BookRepository.java
@@ -1,7 +1,16 @@
 package bookstore.bookstoreapp.model;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
     
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitleContaining(String keyword);
+
+    List<Book> findByPublicationYearOrderByTitleAsc(int year);
+
+    List<Book> findByAuthorIgnoreCase(String author);
 }
--- bookstoreapp/src/main/java/bookstore/bookstoreapp/web/BookController.java
+++ bookstoreapp/src/main/java/bookstore/bookstoreapp/web/BookController.java
@@ -1,28 +1,69 @@
 package bookstore.bookstoreapp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import bookstore.bookstoreapp.model.Book;
 import bookstore.bookstoreapp.model.BookRepository;
 
 @Controller
 public class BookController {
 
-    private final BookRepository repository;
+    @Autowired
+    private BookRepository repository;
 
-    public BookController(BookRepository repository) {
-        this.repository = repository;
-    }
-
-    @GetMapping("/booklist")
+    @RequestMapping(value = "/booklist")
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
-        return "booklist"; 
+        return "booklist";
     }
 
-    @GetMapping("/index")
+    @RequestMapping(value = "/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+   
+    @RequestMapping("/search")
+    public String searchBooks(@RequestParam("author") String author, Model model) {
+        model.addAttribute("books", repository.findByAuthor(author));
+        return "booklist";
+    }
+
+    @RequestMapping(value = "/edit/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        Book book = repository.findById(id).orElse(null);
+        model.addAttribute("book", book);
+        return "addbook"; 
+    }
+
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateBook(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
     }
 }
